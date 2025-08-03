877d150a64e691018733a87148d856832d303fa1
Chesta Ardiona <chestapanduganteng@gmail.com>
2025-08-03 15:08:42 +0700

    update: search context separating getSequelProfile and getProfile

--- src/contexts/SearchContext.tsx
+++ src/contexts/SearchContext.tsx
@@ -1,4 +1,4 @@
-import type { GoalData, SearchProfile, SearchType, TaskData, TError } from "@/types/types";
+import type { GoalData, SearchProfile, TaskData, TError } from "@/types/types";
 import callApi from "@/utils/callApi";
 import { handleError } from "@/utils/errorHandler";
 import { createContext, useState, type ReactNode } from "react";
@@ -9,11 +9,20 @@ export type SearchData = {
   profiles?: SearchProfile[];
   goals?: GoalData[];
   tasks?: TaskData[];
+  initiate?: boolean;
+};
+
+type GetDataProps = {
+  type?: string;
+  query?: string;
+  offset?: number;
+  recycled?: boolean;
 };
 
 type TSearchContext = {
   data: SearchData;
-  getData: () => Promise<void>;
+  getData: (options?: GetDataProps) => Promise<void>;
+  getSequelProfile: (options?: Omit<GetDataProps, "type">) => Promise<void>;
   setData: React.Dispatch<React.SetStateAction<SearchData>>;
   error: TError & { search: string };
   clearError: () => void;
@@ -24,12 +33,13 @@ type TSearchContext = {
   setQuery: React.Dispatch<React.SetStateAction<string>>;
 };
 
-type Params = { type: SearchType; offset: number };
-const defaultParams = { type: "ALL", offset: 0 } as Params;
+type Params = { type: string };
+const defaultParams: Params = { type: "all" };
 
 const SearchContext = createContext<TSearchContext>({
   data: { profiles: [], goals: [], tasks: [] },
   getData: async () => {},
+  getSequelProfile: async () => {},
   setData: () => {},
   error: { error: null, search: "" },
   clearError: () => {},
@@ -41,18 +51,41 @@ const SearchContext = createContext<TSearchContext>({
 });
 
 const SearchProvider = ({ children }: { children: ReactNode }) => {
-  const [data, setData] = useState<SearchData>({ profiles: [], goals: [], tasks: [] });
+  const [data, setData] = useState<SearchData>({ profiles: [], goals: [], tasks: [], initiate: true });
   const [error, setError] = useState<TError & { search: string }>({ error: null, search: "" });
   const [params, setParams] = useState<Params>(defaultParams);
   const [query, setQuery] = useState("");
 
-  const getData = async () => {
-    try {
-      if (!query.trim()) return;
-      const response = await callApi(`/search?query=${query}&type=${params.type}${params.offset ? `&offset=${params.offset}` : ""}`, {
+  const fetch = async (options?: GetDataProps) => {
+    const { type: optionType, offset: optionOffset, query: optionQuery, recycled } = options || {};
+    if (!query.trim()) return;
+    return await callApi(
+      `/search?query=${optionQuery ?? query}&type=${optionType || params.type}&recycled=${!!recycled}${
+        optionOffset ?? data.nextOffset ? `&offset=${optionOffset ?? data.nextOffset}` : ""
+      }`,
+      {
         method: "GET",
-      });
+      }
+    );
+  };
+
+  const getData = async (options?: GetDataProps) => {
+    try {
+      const response = await fetch({ ...options, offset: 0 });
+      if (!response) return;
       setData(response.data);
+    } catch (err) {
+      handleError(err, setError);
+    }
+  };
+
+  const getSequelProfile = async (options?: Omit<GetDataProps, "type">) => {
+    try {
+      const response = await fetch({ ...options, type: "profiles" });
+      if (!response) return;
+      const { data } = response;
+      const { isNext, nextOffset } = data;
+      setData((prev) => ({ ...prev, isNext, nextOffset, profiles: [...(prev.profiles || []), ...data.profiles] }));
     } catch (err) {
       handleError(err, setError);
     }
@@ -62,6 +95,7 @@ const SearchProvider = ({ children }: { children: ReactNode }) => {
   const contextValue: TSearchContext = {
     data,
     getData,
+    getSequelProfile,
     setData,
     error,
     setError,
